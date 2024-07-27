61d5f30941efc367a915b96acf112243e233c227
Max <mxmivlev@gmail.com>
2024-07-27 23:21:14 +0100

    Add working header

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,32 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
-  return (
-      <header>
-        <nav>
-          <Link href="/">Home</Link>
-          <Link href="/contact">Contact</Link>
-          <div className="dropdown">
-            <button className="dropbtn">Tours</button>
-            <div className="dropdown-content">
-              {/* This will be populated by an edge function */}
+    return (
+        <header className="header">
+            <div className="header-container">
+                <div className="logo">
+                    <Link href="/" legacyBehavior>
+                        <a>
+                            <Image src="/logos/lotus_logo_nobg_yellow.png" alt="Logo" width={100} height={50} />
+                        </a>
+                    </Link>
+                </div>
+                <nav className="nav">
+                    <Link href="/" legacyBehavior>
+                        <a className="nav-link">Home</a>
+                    </Link>
+                    <Link href="/contact" legacyBehavior>
+                        <a className="nav-link">Contact</a>
+                    </Link>
+                    <div className="dropdown">
+                        <button className="dropbtn">Tours</button>
+                        <div className="dropdown-content">
+                            {/* This will be populated by an edge function */}
+                        </div>
+                    </div>
+                </nav>
             </div>
-          </div>
-        </nav>
-      </header>
-  );
+        </header>
+    );
 }

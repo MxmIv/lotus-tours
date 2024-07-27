969eb8b307feeaf2961f836fdeb9d8c99de0ff5b
Max <mxmivlev@gmail.com>
2024-07-28 00:08:58 +0100

    Working header tours

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,13 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 
-export default function Contact() {
+export default function Contact({ tours }) {
     return (
         <>
-            <Header />
+            <Header tours={tours} />
             <div className="contact-form">
                 <h1>Contact Us</h1>
                 <form action="/api/contact" method="post">
@@ -17,4 +20,29 @@ export default function Contact() {
             <Footer />
         </>
     );
+}
+
+export async function getStaticProps() {
+    const files = fs.readdirSync(path.join('content/tours'));
+
+    const tours = files.filter((filename) => {
+        const filepath = path.join('content/tours', filename);
+        return fs.statSync(filepath).isFile() && filename.endsWith('.md');
+    }).map(filename => {
+        const markdownWithMeta = fs.readFileSync(
+            path.join('content/tours', filename),
+            'utf-8'
+        );
+        const { data: frontmatter } = matter(markdownWithMeta);
+        return {
+            frontmatter,
+            slug: filename.split('.')[0]
+        };
+    });
+
+    return {
+        props: {
+            tours,
+        },
+    };
 }

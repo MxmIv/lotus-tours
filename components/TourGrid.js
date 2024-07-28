8a0c87462a152179ffaaec6a11beaa2559dccdad
Max <mxmivlev@gmail.com>
2024-07-28 21:14:17 +0100

    add price for grid

--- components/TourGrid.js
+++ components/TourGrid.js
@@ -1,10 +1,19 @@
-import TourCard from './TourCard';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function TourGrid({ tours }) {
     return (
         <div className="tour-grid">
-            {tours.map((tour) => (
-                <TourCard key={tour.slug} tour={tour} />
+            {tours.map((tour, index) => (
+                <div key={index} className="tour-card">
+                    <Image src={`/${tour.frontmatter.images[0]}`} alt={tour.frontmatter.title} width={300} height={200} />
+                    <h2>{tour.frontmatter.title}</h2>
+                    <p>{tour.frontmatter.description}</p>
+                    <p className="price">{tour.frontmatter.price}</p>
+                    <Link href={`/tours/${tour.slug}`} legacyBehavior>
+                        <a>Read More</a>
+                    </Link>
+                </div>
             ))}
         </div>
     );

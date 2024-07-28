87e05eb19e21c36e7cfb28987a7393fed9131f83
Max <mxmivlev@gmail.com>
2024-07-28 21:23:46 +0100

    Revert "add price for grid"
    This reverts commit 8a0c87462a152179ffaaec6a11beaa2559dccdad.

--- components/TourGrid.js
+++ components/TourGrid.js
@@ -1,19 +1,10 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import TourCard from './TourCard';
 
 export default function TourGrid({ tours }) {
     return (
         <div className="tour-grid">
-            {tours.map((tour, index) => (
-                <div key={index} className="tour-card">
-                    <Image src={`/${tour.frontmatter.images[0]}`} alt={tour.frontmatter.title} width={300} height={200} />
-                    <h2>{tour.frontmatter.title}</h2>
-                    <p>{tour.frontmatter.description}</p>
-                    <p className="price">{tour.frontmatter.price}</p>
-                    <Link href={`/tours/${tour.slug}`} legacyBehavior>
-                        <a>Read More</a>
-                    </Link>
-                </div>
+            {tours.map((tour) => (
+                <TourCard key={tour.slug} tour={tour} />
             ))}
         </div>
     );
